3a9df16bc91438e223e4bd7a673457c71bbe132f
andymcwilliams <andrew.mcwilliams95@gmail.com>
2020-12-07 22:33:30 +0000

    Pictures added to home page.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Header from './components/Header';
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-
+import phone from "./images/phone.png";
+import email from "./images/email.png";
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
@@ -15,13 +17,30 @@ function App() {
       <div className="App-body">
         <Grid container spacing={5}>
           <Grid item xs={12}>
-            <Paper>What does this mean</Paper>
+            <Paper>
+            </Paper>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <Paper>xs=12 sm=6</Paper>
+            <Paper>
+              <div>
+                <img src={phone} alt="phone" width="90" />
+                <div>
+                  <Button variant="contained" color="primary" href="tel:02870342281">
+                    call us now!
+                  </Button>
+                </div>
+              </div>
+            </Paper>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <Paper>xs=12 sm=6</Paper>
+            <Paper>
+              <img src={email} alt="mail" width="90" />
+              <div>
+                <Button variant="contained" color="primary" href="mailto:rhkdavidson@aol.com">
+                  rhkdavidson@aol.com
+              </Button>
+              </div>
+            </Paper>
           </Grid>
           <Grid item xs={6} sm={3}>
             <Paper>xs=6 sm=3</Paper>
